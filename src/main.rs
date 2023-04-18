ef721855c5549df1937ee23720590de3bbd36020
S1e7J <s.montoyar2@uniandes.edu.co>
2023-04-17 21:04:34 -0500

    Segundo Push, es un simple script que lee el archivo datos.csv que se encuentra en el directorio en el que se encuentra

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs::File;
+// use std::io::prelude::*;
+use csv::Reader;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Abre el archivo CSV
+    // let mut file = File::open("datos.csv")?;
+    let file = File::open("datos.csv")?;
+
+    // Lee los datos del archivo CSV
+    let mut reader = Reader::from_reader(file);
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
 }
